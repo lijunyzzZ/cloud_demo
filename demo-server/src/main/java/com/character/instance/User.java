7166ae8ca560df325067d16551366f6761cd2290
lijunyi <1920271554@qq.com>
2019-02-14 21:10:02 +0800

    增加数据库连接的util

---
+++ demo-server/src/main/java/com/character/instance/User.java
@@ -0,0 +1,34 @@
+package com.character.instance;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Double sal;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSal() {
+        return sal;
+    }
+
+    public void setSal(Double sal) {
+        this.sal = sal;
+    }
+}
